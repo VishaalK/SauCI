1e1700ec46208259439cce3a59d0889b3a69a7d7
Vishaal Kalwani <vishaalkal@gmail.com>
2019-03-30 17:59:23 -0700

    Made more progress in the tutorial

--- SauCI/polls/models.py
+++ SauCI/polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    # note that this is the definition of the class AS WELL AS the constructor
+    # whereas in C++ you declare the class, and then also define its constructor (and other possible constructors)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
