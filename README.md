f9802d6cce7ca2e6a6c7f38a0cde38aefcca648a
Vishaal Kalwani <vishaalkal@gmail.com>
2019-04-03 21:29:09 -0700

    Added some views to the polls details page on Windows, as well as updated some documentation

---
+++ README.md
@@ -0,0 +1,16 @@
+# Installation Instructions
+
+## Mac
+
+Make sure you have Python3 installed, as well as pip. This should hopefully not be necessary when this is wrapped in a Docker image.
+
+## Windows
+
+Enable Windows Subystem for Linux, and use Bash in a terminal in VS Code so that I can work on both my laptop and my MacBook. Make a small blog post about this cross-platform compatibility (or wrap it in Docker?)
+
+https://docs.microsoft.com/en-us/windows/wsl/install-win10
+
+```
+// get pip on Windows somehow
+pip install Django
+```
--- SauCI/polls/views.py
+++ SauCI/polls/views.py
@@ -2,3 +2,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id) 
+    # What is idiomatic string interpolation in Python? Compare it to C++
+    # At the very least, we know one now. How do different types work, and multiple arguments?
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
