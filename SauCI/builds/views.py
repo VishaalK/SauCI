88a0820e921fdb538fa09fee74fefa8f1beb0a5b
Vishaal Kalwani <vishaalkal@gmail.com>
2019-04-16 22:36:44 -0700

    some basic stuff

--- SauCI/builds/views.py
+++ SauCI/builds/views.py
@@ -19,7 +19,7 @@ def start(request, build_id):
     try:
         build.created_at = datetime.now()
         build.save()
-    except :
+    except:
         return render(request, 'builds/detail.html', {'build': build})
     else:
         return HttpResponseRedirect(reverse('builds:detail', args=(build_id,)))
