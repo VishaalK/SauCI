3474b40131daccb2999eca8df41f2b80dfa91044
Vishaal Kalwani <vishaalkal@gmail.com>
2019-04-17 23:31:42 -0700

    import docker python api

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 import subprocess
+import docker
 from subprocess import PIPE, CalledProcessError
+
+client = docker.from_env()
+client
 
 try:
     subprocess.run(["python3", "test2.py"], check=True, stdout=PIPE, stderr=PIPE)
