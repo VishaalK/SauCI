56f62d421f951def664f2f5606ff57043005d68a
Vishaal Kalwani <vishaalkal@gmail.com>
2019-04-24 21:18:50 -0700

    Loaded a basic YAML string

--- convert.py
+++ convert.py
@@ -1,4 +1,15 @@
-import docker
+# import docker
+from yaml import load, dump
+
+document = """
+  a: 1
+  b:
+    c: 3
+    d: 4
+"""
 
 def produce_docker_file(sauciYmlConfig):
-    return "hi"
+   return "sup"
+
+loaded = load(document)
+print(loaded)
